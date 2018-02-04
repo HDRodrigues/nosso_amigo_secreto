7d838576bfbded67f9f0015058b5e1ea88e0dce4
Helton <helton.dias.rodrigues@gmail.com>
2018-02-04 14:41:33 -0200

    Set job usign service raffle

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module NossoAmigoSecreto
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #configurando a execucao dos jobs para ser pelo sidekiq
+    config.active_job.queue_adapter = :sidekiq
   end
 end
